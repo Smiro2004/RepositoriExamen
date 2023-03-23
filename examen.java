f995ab999c83feae687bdf535c1885f19bded448
Smiro2004 <118193614+Smiro2004@users.noreply.github.com>
2023-03-23 18:50:50 +0100

    Update examen.java

--- examen.java
+++ examen.java
@@ -1,4 +1,4 @@
-public class App {
+public class examen {
     public static void main(String[] args) throws Exception {
         System.out.println("Sóc Sergi Miró i estic fent l'examen de la UF2 del M01");
     }
